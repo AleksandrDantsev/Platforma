a7a6d5784350086f84488921f7912d2741de96a2
Alexander Dantsev <ivan_sidorov_99@internet.ru>
2024-09-05 20:56:13 +0300

    add data table

---
+++ data.js
@@ -0,0 +1,86 @@
+export const data = [
+    { 
+        "Company Name": "Premier Buy", 
+        "City": "Richfield", 
+        "State": "Minnesota", 
+        "Phone": "(612) 291-1000", 
+        "Fax": "(612) 291-1000" 
+    },
+    { 
+        "Company Name": "Techno World", 
+        "City": "San Francisco", 
+        "State": "California", 
+        "Phone": "(415) 555-0199", 
+        "Fax": "(415) 555-0123" 
+    },
+    { 
+        "Company Name": "Global Tech", 
+        "City": "New York", 
+        "State": "New York", 
+        "Phone": "(212) 555-0177", 
+        "Fax": "(212) 555-0166" 
+    },
+    { 
+        "Company Name": "NextGen Solutions", 
+        "City": "Austin", 
+        "State": "Texas", 
+        "Phone": "(512) 555-0145", 
+        "Fax": "(512) 555-0189" 
+    },
+    { 
+        "Company Name": "Innovatech", 
+        "City": "Seattle", 
+        "State": "Washington", 
+        "Phone": "(206) 555-0195", 
+        "Fax": "(206) 555-0174" 
+    },
+    { 
+        "Company Name": "Future Enterprises", 
+        "City": "Chicago", 
+        "State": "Illinois", 
+        "Phone": "(312) 555-0113", 
+        "Fax": "(312) 555-0182" 
+    },
+    { 
+        "Company Name": "Digital Dynamics", 
+        "City": "Boston", 
+        "State": "Massachusetts", 
+        "Phone": "(617) 555-0156", 
+        "Fax": "(617) 555-0147" 
+    },
+    { 
+        "Company Name": "Tech Empire", 
+        "City": "Los Angeles", 
+        "State": "California", 
+        "Phone": "(323) 555-0170", 
+        "Fax": "(323) 555-0139" 
+    },
+    { 
+        "Company Name": "Tech Innovations", 
+        "City": "Denver", 
+        "State": "Colorado", 
+        "Phone": "(303) 555-0161", 
+        "Fax": "(303) 555-0188" 
+    },
+    { 
+        "Company Name": "Quantum Solutions", 
+        "City": "Philadelphia", 
+        "State": "Pennsylvania", 
+        "Phone": "(215) 555-0184", 
+        "Fax": "(215) 555-0133" 
+    },
+    { 
+        "Company Name": "Synergy Tech", 
+        "City": "Dallas", 
+        "State": "Texas", 
+        "Phone": "(214) 555-0118", 
+        "Fax": "(214) 555-0143" 
+    },
+    { 
+        "Company Name": "Elite Technologies", 
+        "City": "San Diego", 
+        "State": "California", 
+        "Phone": "(619) 555-0192", 
+        "Fax": "(619) 555-0128" 
+    }
+  ];
