3ed01479ef2683e00976cf701ac6e1a7b02c6d04
Alexander Dantsev <ivan_sidorov_99@internet.ru>
2024-09-05 21:06:10 +0300

    add configuration

---
+++ src/report-config.js
@@ -0,0 +1,11 @@
+export const config = {
+    name: "Report",
+    code: "report1",
+    columns: [
+      { "dataField": "Company Name", "caption": "Company Name", "dataType": "string" },
+      { "dataField": "City", "caption": "City", "dataType": "string" },
+      { "dataField": "State", "caption": "State", "dataType": "string" },
+      { "dataField": "Phone", "caption": "Phone", "dataType": "string" },
+      { "dataField": "Fax", "caption": "Fax", "dataType": "string" }
+    ]
+  };
