94137dbb3a1c52d0d36b0196f4978baac1bbf0b2
Alexander Dantsev <ivan_sidorov_99@internet.ru>
2024-09-05 21:07:02 +0300

    add debounce func

---
+++ src/helpers/debounce.js
@@ -0,0 +1,10 @@
+export function debounce(func, delay) {
+    let timerId;
+    
+    return function(...args) {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+      timerId = setTimeout(() => func(...args), delay);
+    };
+  }
